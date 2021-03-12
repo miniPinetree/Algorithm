e67c351a993fa2e254fe0773847034b9c8d5fac0
go <65996346+minigomi@users.noreply.github.com>
2021-03-12 13:12:09 +0900

    algorithm

---
+++ 11053.py
@@ -0,0 +1,19 @@
+
+# 접근방식
+# 1. 수열에 나온 값은 메모이제이션하고 존재하면 건너뛰고 없으면 갱신하기
+# 2. max 값을 갱신하면서 카운트하는 방법
+
+memo = []
+
+def increasing_num_dynamic_programming(n, memo):
+    if n in memo:
+        return
+    max_num = 
+    fibo_memo[n] = nth_fibo
+    return nth_fibo
+
+n = int(input())
+num_list = list(map(int,input().split()))
+
+for a in num_list:
+    increasing_num_dynamic_programming(a,memo)
