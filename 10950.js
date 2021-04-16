cf5e208553622ac8a59fc95e71dd74e0d6a6c61d
go <misong321@gmail.com>
2021-04-17 00:47:41 +0900

    algoritm

--- 10950.js
+++ 10950.js
@@ -1,9 +1,19 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
-rl.on('line', function(line){
-    const 
-})
+let input = [];
+
+rl.on("line", function (line) {
+  line = line.split(" ");
+  input.push(line);
+}).on("close", function () {
+  input.map((i) => {
+    if (i.length > 1) {
+      console.log(Number(i[0]) + Number(i[1]));
+    }
+  });
+  process.exit();
+});
