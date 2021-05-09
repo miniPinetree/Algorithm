7d7bb9ebe4457d010bcd53458b23490d45c5a5de
Misong Go <68773118+miniPinetree@users.noreply.github.com>
2021-05-09 15:17:21 +0900

    Create 예상대진표.js
    프로그래머스 예상대진표

---
+++ 예상대진표.js
@@ -0,0 +1,13 @@
+// 프로그래머스 예상대진표 JS
+
+function solution(n,a,b)
+{
+    let cnt = 1;
+    while(Math.ceil(a/2)!==Math.ceil(b/2)){
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        cnt++;
+    }
+    
+    return cnt;
+}
